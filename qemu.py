6d05736e2ce79ece7d10da056dd3e57f67b9814a
Nolan Leake <nolan@sigbus.net>
2021-03-28 14:25:58 -0700

    Support for running the booting the image in qemu.

---
+++ qemu.py
@@ -0,0 +1,45 @@
+import os, subprocess
+
+import util
+
+#XXX x86-64 support
+qemu = 'qemu-system-aarch64'
+nbdkit = 'nbdkit'
+dtb_filename = 'bcm2837-rpi-3-b.dtb'
+
+SLOP = 256 * 1024 * 1024
+
+def next_power_of_2(n):
+    k = 1
+    while k < n:
+        k = k << 1
+    return k
+
+def qemu_run(imgdir):
+    with open(os.path.join(imgdir, 'config.txt'), 'r') as cfgf:
+        for l in cfgf.readlines():
+            if l.startswith('kernel='):
+                kernel = os.path.join(imgdir, l.split('=')[1]).strip()
+            elif l.startswith('initramfs '):
+                initrd = os.path.join(imgdir, l.split(' ')[1]).strip()
+    dtb = os.path.join(imgdir, dtb_filename)
+
+    with open(os.path.join(imgdir, 'cmdline.txt'), 'r') as cmdlinef:
+        kernel_args = cmdlinef.readlines()[0].strip()
+
+    #sdcards must be a power of 2, so we figure out the size of the dir, add some slop, then
+    # pick the next highest power of 2.
+    size = int(subprocess.check_output(['du', '-sb', imgdir], universal_newlines=True).split()[0])
+    size = next_power_of_2(size + SLOP)
+
+    qemu_args = [qemu, '-M', 'raspi3b', '-m', '1024',
+                 '-kernel', kernel, '-dtb', dtb, '-initrd', initrd, '-append', kernel_args,
+                 '-sd', '$nbd', '-nographic',
+                 '-chardev', 'stdio,id=char0,mux=on,signal=off',
+                 '-serial', 'chardev:char0', '-serial', 'chardev:char0', '-mon', 'chardev=char0']
+    qemu_args = util.posix_list2cmdline(qemu_args).replace("'$nbd'", '$nbd')
+    args = [nbdkit, '-U', '-', '--filter=truncate', '--filter=cow',
+            'floppy', 'dir=%s' % imgdir, 'truncate=%s' % size,
+            '--run', qemu_args]
+    print('\n#####################\nctrl-a x to exit qemu\n#####################\n')
+    os.execvp(nbdkit, args)
