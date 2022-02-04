edca0c64c40ab5dcf822854128970da41a1814ea
Nolan Leake <nolan@sigbus.net>
2022-02-03 16:36:54 -0800

    Support writing to the SDCard.
    All writes are ephemeral, and will be lost if the qemu VM restarts.
    
    If a new enough nbdkit is not available, writing will not work.

--- qemu.py
+++ qemu.py
@@ -1,4 +1,4 @@
-import os, subprocess
+import os, sys, subprocess
 
 import util
 
@@ -7,13 +7,10 @@ qemu = os.environ.get('QEMU', 'qemu-system-aarch64')
 nbdkit = os.environ.get('NBDKIT', 'nbdkit')
 dtb_filename = os.environ.get('DTB', 'bcm2710-rpi-3-b.dtb')
 
-SLOP = 256 * 1024 * 1024
-
-def next_power_of_2(n):
-    k = 1
-    while k < n:
-        k = k << 1
-    return k
+def _recent_nbdkit():
+    verstr = subprocess.run(['nbdkit', '--version'], capture_output=True).stdout.decode()[7:-1]
+    major, minor, patch = [int(i) for i in verstr.split('.')]
+    return major > 1 or (major == 1 and minor > 27) or (major == 1 and minor == 27 and patch > 8)
 
 def qemu_run(imgdir, cmd=None):
     dtb = os.path.join(imgdir, dtb_filename)
@@ -22,10 +19,8 @@ def qemu_run(imgdir, cmd=None):
     if cmd is not None:
         kernel_args += 'XQEMUXcmd_to_eol=%s' % cmd
 
-    #sdcards must be a power of 2, so we figure out the size of the dir, add some slop, then
-    # pick the next highest power of 2.
-    size = int(subprocess.check_output(['du', '-sb', imgdir], universal_newlines=True).split()[0])
-    size = next_power_of_2(size + SLOP)
+    #sdcards must be a power of 2.
+    size = 16 * 1024 * 1024 * 1024
 
     qemu_args = [qemu, '-M', 'raspi3b', '-m', '1024',
                  '-kernel', os.path.join(imgdir, 'u-boot.bin'), '-dtb', dtb, '-append', kernel_args,
@@ -35,8 +30,15 @@ def qemu_run(imgdir, cmd=None):
                  '-nographic', '-chardev', 'stdio,id=char0,mux=on,signal=off',
                  '-serial', 'chardev:char0', '-serial', 'chardev:char0', '-mon', 'chardev=char0']
     qemu_args = util.posix_list2cmdline(qemu_args).replace('\\$nbd', '$nbd')
-    args = [nbdkit, '-U', '-', '--filter=truncate', '--filter=cow',
-            'floppy', 'dir=%s' % imgdir, 'truncate=%s' % size,
-            '--run', qemu_args]
+    if _recent_nbdkit():
+        args = [nbdkit, '-U', '-', '--filter=cow',
+                'floppy', 'dir=%s' % imgdir, 'size=%s' % size,
+                '--run', qemu_args]
+    else:
+        print('WARNING: Your nbdkit is too old to support writing to the SDCard,'
+              ' you need 1.27.9 or newer.', file=sys.stderr)
+        args = [nbdkit, '-U', '-', '--filter=truncate', '--filter=cow',
+                'floppy', 'dir=%s' % imgdir, 'truncate=%s' % size,
+                '--run', qemu_args]
     print('\n#####################\nctrl-a x to exit qemu\n#####################\n')
     os.execvp(nbdkit, args)
