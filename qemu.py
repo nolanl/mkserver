e342960cb18bcb9a1931b48b661adbf16728dd01
Nolan Leake <nolan@sigbus.net>
2022-02-10 15:47:34 -0800

    Check and warn about too old qemu.

--- qemu.py
+++ qemu.py
@@ -1,4 +1,4 @@
-import os, sys, subprocess
+import os, sys, subprocess, re
 
 import util
 
@@ -7,10 +7,12 @@ qemu = os.environ.get('QEMU', 'qemu-system-aarch64')
 nbdkit = os.environ.get('NBDKIT', 'nbdkit')
 dtb_filename = os.environ.get('DTB', 'bcm2710-rpi-3-b.dtb')
 
-def _recent_nbdkit():
-    verstr = subprocess.run(['nbdkit', '--version'], capture_output=True).stdout.decode()[7:-1]
-    major, minor, patch = [int(i) for i in verstr.split('.')]
-    return major > 1 or (major == 1 and minor > 27) or (major == 1 and minor == 27 and patch > 8)
+semver = re.compile('[0-9]+[.][0-9]+[.][0-9]+')
+def _recent_program(program, needmajor, needminor, needpatch):
+    verstr = subprocess.run([program, '--version'], capture_output=True).stdout.decode()
+    major, minor, patch = [int(i) for i in semver.search(verstr)[0].split('.')]
+    return major > needmajor or (major == needmajor and minor > needminor) or \
+        (major == needmajor and minor == needminor and patch >= needpatch)
 
 def qemu_run(imgdir, cmd=None):
     dtb = os.path.join(imgdir, dtb_filename)
@@ -30,13 +32,19 @@ def qemu_run(imgdir, cmd=None):
                  '-nographic', '-chardev', 'stdio,id=char0,mux=on,signal=off',
                  '-serial', 'chardev:char0', '-serial', 'chardev:char0', '-mon', 'chardev=char0']
     qemu_args = util.posix_list2cmdline(qemu_args).replace('\\$nbd', '$nbd')
-    if _recent_nbdkit():
+
+    recent_nbdkit, recent_qemu = _recent_program(nbdkit, 1, 27, 2), _recent_program(qemu, 6, 1, 0)
+    if recent_nbdkit and recent_qemu:
         args = [nbdkit, '-U', '-', '--filter=cow',
                 'floppy', 'dir=%s' % imgdir, 'size=%s' % size,
                 '--run', qemu_args]
     else:
-        print('WARNING: Your nbdkit is too old to support writing to the SDCard,'
-              ' you need 1.27.9 or newer.', file=sys.stderr)
+        if not recent_nbdkit:
+            print('WARNING: Your nbdkit is too old to support writing to the SDCard,'
+                  ' so mkserver --update will not work. You need 1.27.2 or newer.', file=sys.stderr)
+        if not recent_qemu:
+            print('WARNING: Your qemu is too old to support soft rebooting rapsberry'
+                  ' pis, so mkserver --update will not work. You need 6.1.0 or newer', file=sys.stderr)
         args = [nbdkit, '-U', '-', '--filter=truncate', '--filter=cow',
                 'floppy', 'dir=%s' % imgdir, 'truncate=%s' % size,
                 '--run', qemu_args]
